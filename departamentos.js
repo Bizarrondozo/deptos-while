34b366315b487935dd71f2b282a6c4ce244d0c57
Juan Felipe Ramirez <juanfeliperamirezsanchez@gmail.com>
2021-07-28 10:17:13 -0500

    Departamentos con while

---
+++ departamentos.js
@@ -0,0 +1,414 @@
+let amazonas = document.getElementById("amazonas");
+let antioquia = document.getElementById("antioquia");
+let arauca = document.getElementById("arauca");
+let bolivar = document.getElementById("bolivar");
+let boyaca = document.getElementById("boyaca");
+let caldas = document.getElementById("caldas");
+let caqueta = document.getElementById("caqueta");
+let casanare = document.getElementById("casanare");
+let cauca = document.getElementById("cauca");
+let cesar = document.getElementById("cesar");
+let choco = document.getElementById("choco");
+let cordoba = document.getElementById("cordoba");
+let cundinamarca = document.getElementById("cundinamarca");
+let guainia = document.getElementById("guainia");
+let guaviare = document.getElementById("guaviare");
+let huila = document.getElementById("huila");
+let guajira = document.getElementById("guajira");
+let magdalena = document.getElementById("magdalena");
+let meta = document.getElementById("meta");
+let narino = document.getElementById("narino");
+let nsantander = document.getElementById("nsantander");
+let putumayo = document.getElementById("putumayo");
+let quindio = document.getElementById("quindio");
+let risaralda = document.getElementById("risaralda");
+let andres = document.getElementById("andres");
+let santander = document.getElementById("santander");
+let sucre = document.getElementById("sucre");
+let tolima = document.getElementById("tolima");
+let vcauca = document.getElementById("vcauca");
+let vaupes = document.getElementById("vaupes");
+let vichada = document.getElementById("vichada");
+
+amazonas.onclick = function() {
+    let opAmazonas = prompt("Cual es la capital de Amazonas");
+    while (opAmazonas !== "Leticia") {
+        alert("Mal, vuelve a intentarlo");
+        let opAmazonas = prompt("Cual es la capital de Amazonas");
+        if (opAmazonas === "Leticia") {
+            return confirm("La capital de amazonas es Leticia, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+antioquia.onclick = function() {
+    let opAntioquia = prompt("Cual es la capital de Antioquia");
+    while (opAntioquia !== "Medellin") {
+        alert("Mal, vuelve a intentarlo");
+        let opAntioquia = prompt("Cual es la capital de Antioquia");
+        if (opAntioquia === "Medellin") {
+            return confirm("La capital de Antioquia es Medellin, quieres saber mas de esta ciudad?")
+        }
+    }
+
+};
+
+arauca.onclick = function() {
+    let opArauca = prompt("Cual es la capital de Arauca");
+    while (opArauca !== "Arauca") {
+        alert("Mal, vuelve a intentarlo");
+        let opArauca = prompt("Cual es la capital de Arauca");
+        if (opArauca === "Arauca") {
+            return confirm("La capital de Arauca es Arauca, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+atlantico.onclick = function() {
+    let opAtlantico = prompt("Cual es la capital de Atlantico");
+    while (opAtlantico !== "Barranquilla") {
+        alert("Mal, vuelve a intentarlo");
+        let opAtlantico = prompt("Cual es la capital de Atlantico");
+        if (opAtlantico === "Barranquilla") {
+            return confirm("La capital de Atlantico es Barranquilla, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+bolivar.onclick = function() {
+    let opBolivar = prompt("Cual es la capital de Bolivar");
+    while (opBolivar !== "Cartagena") {
+        alert("Mal, vuelve a intentarlo");
+        let opBolivar = prompt("Cual es la capital de Bolivar");
+        if (opBolivar === "Cartagena") {
+            return confirm("La capital de Bolivar es Cartagena, quieres saber mas de esta ciudad?")
+        }
+    }
+
+};
+
+boyaca.onclick = function() {
+    let opBoyaca = prompt("Cual es la capital de Boyaca");
+    while (opBoyaca !== "Tunja") {
+        alert("Mal, vuelve a intentarlo");
+        let opBoyaca = prompt("Cual es la capital de Boyaca");
+        if (opBoyaca === "Tunja") {
+            return confirm("La capital de Boyaca es Tunja, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+caldas.onclick = function() {
+    let opCaldas = prompt("Cual es la capital de Caldas");
+    while (opCaldas !== "Manizales") {
+        alert("Mal, vuelve a intentarlo");
+        let opCaldas = prompt("Cual es la capital de Caldas");
+        if (opCaldas === "Manizales") {
+            return confirm("La capital de Caldas es Manizales, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+caqueta.onclick = function() {
+    let opCaqueta = prompt("Cual es la capital de Caqueta");
+    while (opCaqueta !== "Florencia") {
+        alert("Mal, vuelve a intentarlo");
+        let opCaqueta = prompt("Cual es la capital de Caqueta");
+        if (opCaqueta === "Florencia") {
+            return confirm("La capital de Caqueta es Florencia, quieres saber mas de esta ciudad?")
+        }
+    }
+
+};
+
+casanare.onclick = function() {
+    let opCasanare = prompt("Cual es la capital de Casanare");
+    while (opCasanare !== "Yopal") {
+        alert("Mal, vuelve a intentarlo");
+        let opCasanare = prompt("Cual es la capital de Casanare");
+        if (opCasanare === "Yopal") {
+            return confirm("La capital de Casanare es Yopal, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+cauca.onclick = function() {
+    let opCauca = prompt("Cual es la capital de Cauca");
+    while (opCauca !== "Popayán") {
+        alert("Mal, vuelve a intentarlo");
+        let opCauca = prompt("Cual es la capital de Cauca");
+        if (opCauca === "Popayán") {
+            return confirm("La capital de Cauca es Popayán, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+cesar.onclick = function() {
+    let opCesar = prompt("Cual es la capital de Cesar");
+    while (opCesar !== "Valledupar") {
+        alert("Mal, vuelve a intentarlo");
+        let opCesar = prompt("Cual es la capital de Cesar");
+        if (opCesar === "Valledupar") {
+            return confirm("La capital de Cesar es Valledupar, quieres saber mas de esta ciudad?")
+        }
+    }
+
+};
+
+choco.onclick = function() {
+    let opChoco = prompt("Cual es la capital de Choco");
+    while (opChoco !== "Quibdo") {
+        alert("Mal, vuelve a intentarlo");
+        let opChoco = prompt("Cual es la capital de Choco");
+        if (opChoco === "Quibdo") {
+            return confirm("La capital de Choco es Quibdo, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+cordoba.onclick = function() {
+    let opCordoba = prompt("Cual es la capital de Cordoba");
+    while (opCordoba !== "Monteria") {
+        alert("Mal, vuelve a intentarlo");
+        let opCordoba = prompt("Cual es la capital de Cordoba");
+        if (opCordoba === "Monteria") {
+            return confirm("La capital de Cordoba es Monteria, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+cundinamarca.onclick = function() {
+    let opCundinamarca = prompt("Cual es la capital de Cundinamarca");
+    while (opCundinamarca !== "Bogota") {
+        alert("Mal, vuelve a intentarlo");
+        let opCundinamarca = prompt("Cual es la capital de Cundinamarca");
+        if (opCundinamarca === "Bogota") {
+            return confirm("La capital de Cundinamarca es Bogota, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+guainia.onclick = function() {
+    let opGuainia = prompt("Cual es la capital de Guainia");
+    while (opGuainia !== "Inirida") {
+        alert("Mal, vuelve a intentarlo");
+        let opGuainia = prompt("Cual es la capital de Guainia");
+        if (opGuainia === "Inirida") {
+            return confirm("La capital de Guainia es Inirida, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+guaviare.onclick = function() {
+    let opGuaviare = prompt("Cual es la capital de Guaviare");
+    while (opGuaviare !== "San Jose Del Guaviare") {
+        alert("Mal, vuelve a intentarlo");
+        let opGuaviare = prompt("Cual es la capital de Guaviare");
+        if (opGuaviare === "San Jose Del Guaviare") {
+            return confirm("La capital de Guaviare es San Jose Del Guaviare, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+huila.onclick = function() {
+    let opHuila = prompt("Cual es la capital de Huila");
+    while (opHuila !== "Neiva") {
+        alert("Mal, vuelve a intentarlo");
+        let opHuila = prompt("Cual es la capital de Huila");
+        if (opHuila === "Neiva") {
+            return confirm("La capital de Huila es Neiva, quieres saber mas de esta ciudad?")
+        }
+    }
+
+};
+
+guajira.onclick = function() {
+    let opGuajira = prompt("Cual es la capital de Guajira");
+    while (opGuajira !== "Riohacha") {
+        alert("Mal, vuelve a intentarlo");
+        let opGuajira = prompt("Cual es la capital de Guajira");
+        if (opGuajira === "Riohacha") {
+            return confirm("La capital de Guajira es Riohacha, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+magdalena.onclick = function() {
+    let opMagdalena = prompt("Cual es la capital de Magdalena");
+    while (opMagdalena !== "Santa Marta") {
+        alert("Mal, vuelve a intentarlo");
+        let opMagdalena = prompt("Cual es la capital de Magdalena");
+        if (opMagdalena === "Santa Marta") {
+            return confirm("La capital de Magdalena es Santa Marta, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+meta.onclick = function() {
+    let opMeta = prompt("Cual es la capital de Meta");
+    while (opMeta !== "Villavicencio") {
+        alert("Mal, vuelve a intentarlo");
+        let opMeta = prompt("Cual es la capital de Meta");
+        if (opMeta === "Villavicencio") {
+            return confirm("La capital de Meta es Villavicencio, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+narino.onclick = function() {
+    let opNarino = prompt("Cual es la capital de Nariño");
+    while (opNarino !== "Pasto") {
+        alert("Mal, vuelve a intentarlo");
+        let opNarino = prompt("Cual es la capital de Nariño");
+        if (opNarino === "Pasto") {
+            return confirm("La capital de Nariño es Pasto, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+nsantander.onclick = function() {
+    let opNsantander = prompt("Cual es la capital de Norte de Santander");
+    while (opNsantander !== "Cucuta") {
+        alert("Mal, vuelve a intentarlo");
+        let opNsantander = prompt("Cual es la capital de Norte de Santander");
+        if (opNsantander === "Cucuta") {
+            return confirm("La capital de Norte de Santander es Cucuta, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+putumayo.onclick = function() {
+    let opPutumayo = prompt("Cual es la capital de Putumayo");
+    while (opPutumayo !== "Mocoa") {
+        alert("Mal, vuelve a intentarlo");
+        let opPutumayo = prompt("Cual es la capital de Putumayo");
+        if (opPutumayo === "Mocoa") {
+            return confirm("La capital de Putumayo es Mocoa, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+quindio.onclick = function() {
+    let opQuindio = prompt("Cual es la capital de Quindio");
+    while (opQuindio !== "Armenia") {
+        alert("Mal, vuelve a intentarlo");
+        let opQuindio = prompt("Cual es la capital de Quindio");
+        if (opQuindio === "Armenia") {
+            return confirm("La capital de Quindio es Armenia, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+risaralda.onclick = function() {
+    let opRisaralda = prompt("Cual es la capital de Risaralda");
+    while (opRisaralda !== "Pereira") {
+        alert("Mal, vuelve a intentarlo");
+        let opRisaralda = prompt("Cual es la capital de Risaralda");
+        if (opRisaralda === "Pereira") {
+            return confirm("La capital de Risaralda es Pereira, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+andres.onclick = function() {
+    let opAndres = prompt("Cual es la capital de San Andres y Providencia");
+    while (opAndres !== "San Andres") {
+        alert("Mal, vuelve a intentarlo");
+        let opAndres = prompt("Cual es la capital de San Andres y Providencia");
+        if (opAndres === "San Andres") {
+            return confirm("La capital de San Andres y Providencia es San Andres, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+santander.onclick = function() {
+    let opSantander = prompt("Cual es la capital de Santander");
+    while (opSantander !== "Bucaramanga") {
+        alert("Mal, vuelve a intentarlo");
+        let opSantander = prompt("Cual es la capital de Santander");
+        if (opSantander === "Bucaramanga") {
+            return confirm("La capital de Santander es Bucaramanga, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+sucre.onclick = function() {
+    let opSucre = prompt("Cual es la capital de Sucre");
+    while (opSucre !== "Sincelejo") {
+        alert("Mal, vuelve a intentarlo");
+        let opSucre = prompt("Cual es la capital de Sucre");
+        if (opSucre === "Sincelejo") {
+            return confirm("La capital de Sucre es Sincelejo, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+tolima.onclick = function() {
+    let opTolima = prompt("Cual es la capital de Tolima");
+    while (opTolima !== "Ibague") {
+        alert("Mal, vuelve a intentarlo");
+        let opTolima = prompt("Cual es la capital de Tolima");
+        if (opTolima === "Ibague") {
+            return confirm("La capital de Tolima es Ibague, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+vcauca.onclick = function() {
+    let opVcauca = prompt("Cual es la capital de Valle del Cauca");
+    while (opVcauca !== "Cali") {
+        alert("Mal, vuelve a intentarlo");
+        let opVcauca = prompt("Cual es la capital de Valle del Cauca");
+        if (opVcauca === "Cali") {
+            return confirm("La capital de Valle del Cauca es Cali, quieres saber mas de esta ciudad?")
+        }
+    }
+};
+
+vaupes.onclick = function() {
+    let opVaupes = prompt("Cual es la capital de Vaupes");
+    while (opVaupes !== "Mitu") {
+        alert("Mal, vuelve a intentarlo");
+        let opVaupes = prompt("Cual es la capital de Vaupes");
+        if (opVaupes === "Mitu") {
+            return confirm("La capital de Vaupes es Mitu, quieres saber mas de esta ciudad?")
+        }
+
+    }
+
+};
+
+vichada.onclick = function() {
+    let opVichada = prompt("Cual es la capital de Vichada");
+    while (opVichada !== "Puerto Carreño") {
+        alert("Mal, vuelve a intentarlo");
+        let opVichada = prompt("Cual es la capital de Vichada");
+        if (opVichada === "Puerto Carreño") {
+            return confirm("La capital de Vichada es Puerto Carreño, quieres saber mas de esta ciudad?")
+        }
+    }
+};
